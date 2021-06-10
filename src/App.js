b7b0285fde0a1fcd4bf57c506693d59c2c84c1e4
bhumika ✿ <bhumika@bhumikas-MacBook-Air.local>
2021-06-10 18:31:58 -0400

    Add Bhumika's name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { BowlingBall } from '@styled-icons/boxicons-regular/BowlingBall';
 const names = [
   'Sharon He',
   'Jilly Song',
-  'Name X'
+  'Name X',
+  'Bhumika Gupta'
 ]
 
 const App = () => {
